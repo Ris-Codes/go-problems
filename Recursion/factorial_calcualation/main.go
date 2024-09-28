81d61950c465db5beb4c81190c87bb26688721a4
Ris-Codes <riscodes@gmail.com>
2024-09-28 12:33:42 +0530

    Factrail Calculation

---
+++ Recursion/factorial_calcualation/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 10
+	fmt.Println("Factorial of", n, "is", factorial(n))
+}
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
