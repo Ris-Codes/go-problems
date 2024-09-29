eeaea1d12fc67165008609e765e2f2c0f92dc5e9
Ris-Codes <riscodes@gmail.com>
2024-09-29 10:11:39 +0530

    Merge Sort

---
+++ Sorting/merge_sort/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{45, 56, 76, 23, 78, 48, 87}
+	fmt.Println("Sorted array:", mergeSort(arr))
+}
+
+func mergeSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+	left := mergeSort(arr[:mid])
+	right := mergeSort(arr[mid:])
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := []int{}
+	i, j := 0, 0
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+	return result
+}
